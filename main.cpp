4ef56bc3cbfaab93742af8ce03d936162ef24ccc
qvap <qvapelsin@gmail.com>
2025-05-13 22:55:12 +0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int* linear_search (int* begin, int* end, int value) {
+    return nullptr;
+}
+
+int* binary_search (int* begin, int* end, int value) {
+    return nullptr;
+}
+
+int main() {
+    return 0;
+}
